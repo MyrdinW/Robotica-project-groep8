8001390eab0d80e8c83d93bdfd2c39e3cd0f8608
MyrdinW <myrdinw@gmail.com>
2021-05-26 13:05:37 +0200

    minor update LineDance movements
    minor update LineDance movements

--- LineDance.py
+++ LineDance.py
@@ -4,19 +4,28 @@ class LineDance:
     through the # of leds, which references to a so called beat. 
     (Needs upgrade according to addition of gripper)
     """
+
+    def __init__(self):
+        super().__init__()
+        print("Dance initialized")
+
     def get_movement(self,i,x,y):
         movement = (0,0)
 
         if i >= 4:
             if x >= 0 and y >= 0:
                 movement = (-1, -1)
+                print("moving to the back")
 
             elif x >= 0 and y < 0:
                 movement = (-1, 1)
+                print("moving to the left")
 
             elif x < 0 and y >= 0:
                 movement = (1, -1)
+                print("moving to the right")
 
             else:
                 movement = (1, 1)
+                "moving forward"
         return movement
