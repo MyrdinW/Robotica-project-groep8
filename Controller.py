b329424e640fda8baac5b613b9e39e2ed07ed219
jessenaps <jessegerrit@gmail.com>
2021-05-27 13:01:51 +0200

    Ledshow versimpeld

---
+++ Controller.py
@@ -0,0 +1,68 @@
+import datetime
+import threading
+import time
+
+from Camera import Camera
+from Engine import Engine
+from Light import Light
+from Microphone import Microphone
+from Receiver import Receiver
+from Servo import Servo
+from Weight import Weight
+
+
+class Controller:
+    """
+        Controller handles all actions of the robot as a whole.
+        It acts as a facade that handles all components
+    """
+
+    def __init__(self):
+        self.__task = False
+        self.__engine = Engine(3, 2, 4)
+        self.__servo = Servo()
+        self.__microphone = Microphone()
+        self.__light = Light(15)
+        self.__camera = Camera()
+        self.__weight = Weight()
+        self.__receiver = Receiver(self.__camera, self.__microphone)
+        threading.Thread(target=self.listen).start()
+        threading.Thread(target=self.dance).start()
+
+    # Listens for command from the remote
+    def listen(self):
+        while True:
+            command = self.__receiver.listen()
+            if command is not None:
+                words = command.split()
+                if words[0] == "move":
+                    self.move(words[1], words[2])
+                elif words[0] == "movegripper":
+                    self.movegripper(words[1], words[2])
+            time.sleep(0.1)
+
+    # Moves gripper with x and y value of joystick
+    def movegripper(self, x, y):
+        print("Moving gripper")
+
+    # Moves robot with x and y value of joystick
+    def move(self, jsx1, jsy1):
+        value = (int(jsx1) - 2048) / 2048
+        if -0.02 > value > -0.06:
+            value = 0
+        self.__engine.set_value(value)
+
+    # Robot dance command
+    def dance(self):
+        print("dance started")
+        timedelta = datetime.timedelta(minutes=100)
+        timeend = datetime.datetime.now() + timedelta
+        while datetime.datetime.now() < timeend:
+            low, mid, high = self.__microphone.get_max_lights()
+            self.__light.set_values(low, mid, high)
+        self.__light.reset_lights()
+        print("mic stopped")
+
+    # returns all components
+    def get_components(self):
+        return self.__camera, self.__servo, self.__light, self.__engine, self.__microphone, self.__weight
---
+++ Microphone.py
@@ -0,0 +1,53 @@
+import base64
+import io
+import matplotlib.pyplot as plt
+import numpy as np
+import pyaudio
+
+
+class Microphone:
+    """
+    Microphone handles all actions with the microphone
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.__output = []
+        self.__rate = 44100
+        self.__chunk = int(self.__rate / 20)
+        self.__audio = pyaudio.PyAudio()
+        self.__stream = (self.__audio.open(format=pyaudio.paInt16, channels=1, rate=self.__rate, input=True,
+                                           frames_per_buffer=self.__chunk))
+        print("Microphone initialized")
+
+    # returns encoded image of sound waves
+    def get_image(self):
+        data = np.frombuffer(self.__stream.read(self.__chunk), dtype=np.int16)
+        fig, axis = plt.subplots()
+        axis.plot(data, 'r')
+        axis.grid()
+        axis.set_ylim([-10000, 10000])
+        image = io.BytesIO()
+        fig.savefig(image, format="jpg")
+        image.seek(0)
+        encoded_image = base64.b64encode(image.read())
+        return encoded_image
+
+    # Gets amounts of lights to be on for each frequency range
+    def get_max_lights(self):
+        data = np.frombuffer(self.__stream.read(self.__chunk), dtype=np.int16)
+        peak_low = max(data[:200])
+        peak_mid = max(data[200:2000])
+        peak_high = max(data[2000:])
+
+        leds_low = self.get_amount(peak_low)
+        leds_mid = self.get_amount(peak_mid)
+        leds_high = self.get_amount(peak_high)
+        return leds_low, leds_mid, leds_high
+
+    # return amount of lights according to peak of  each frequency range
+    def get_amount(self, peak):
+        lights = abs(1 * int(100 * peak / 2 ** 15))
+        if lights > 5:
+            return 5
+        return lights
