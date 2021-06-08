163c0942cb0dc9449e9a262df252a6f88f8dc870
jessenaps <jessegerrit@gmail.com>
2021-06-08 11:47:16 +0200

    comments toegevoegd
    comments van jesse, koen en jarno

--- Inlever moment code/Sound.py
+++ Inlever moment code/Sound.py
@@ -4,21 +4,23 @@ import random
 
 
 class Sound:
+    """
+    Sound handles all sound coming from the robot, it uses the playsound library to play sounds
+    """
     def __init__(self):
         self.__r2d2 = os.listdir("sounds/r2d2")
         self.__relevant = os.listdir("sounds/makes_sense")
         self.__nsfw = os.listdir("sounds/nsfs")
-    
+
+    # random robot sound
     def random_robot(self):
-        # playsound.playsound(f"sounds/rd2d/{random.choice(self.__r2d2)}")
-        #os.system(f"mpg321 sounds/rd2d/{random.choice(self.__r2d2)}.mp3")
-        # playsound.playsound(f"sounds/r2d2/test.mp3")
-        os.system(f"mpg321 sounds/r2d2/integratie.mp3")
+        playsound.playsound(f"sounds/rd2d/{random.choice(self.__r2d2)}")
+        # os.system(f"mpg321 sounds/r2d2/integratie.mp3")
+
+    # random funny sound
     def random_relevant(self):
         playsound.playsound(f"sounds/makes_sense/{random.choice(self.__relevant)}")
-    
+
+    # random nsfs sound
     def random_nsfs(self):
         playsound.playsound(f"sounds/nsfs/{random.choice(self.__nsfw)}")
-
-sound  = Sound()
-sound.random_robot()
